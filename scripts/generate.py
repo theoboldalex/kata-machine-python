deefc228247152f8bc4cae3f5854c4fc3c623f26
theoboldalex <theoboldalex@gmail.com>
2022-12-14 21:14:46 +0000

    Generate can create functions

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,1 +1,9 @@
-print("Generating...")
+import sys
+
+src_path = f"{sys.path[0]}/../src/" # TODO: update path depending on day generated
+
+def create_function(name, item):
+    f = open(f"{src_path}{name}.py", "w")
+    f.write(f"def {item['fn_name']}({item['fn_args']}) -> {item['return_type']}:\n"
+            f"    pass")
+    f.close()
