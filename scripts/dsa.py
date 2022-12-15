f7280fb944cefcaa5135e6a870185c0354736d3f
theoboldalex <theoboldalex@gmail.com>
2022-12-15 19:50:00 +0000

    Bootstraps classes and functions on make generate

--- scripts/dsa.py
+++ scripts/dsa.py
@@ -3,15 +3,27 @@ DSA = {
         "type": "class"
     },
     "LinearSearchList": {
-        "type": "fn"
+        "type": "fn",
+        "fn_name": "linear_search",
+        "fn_args": "nums: list, target: int",
+        "return_type": "bool"
     },
     "BinarySearchList": {
-        "type": "fn"
+        "type": "fn",
+        "fn_name": "binary_search",
+        "fn_args": "nums: list, target: int",
+        "return_type": "bool"
     },
     "TwoCrystalBalls": {
-        "type": "fn"
+        "type": "fn",
+        "fn_name": "two_crystal_balls",
+        "fn_args": "nums: list",
+        "return_type": "int"
     },
     "BubbleSort": {
-        "type": "fn"
+        "type": "fn",
+        "fn_name": "bubble_sort",
+        "fn_args": "nums: list",
+        "return_type": "list"
     }
 }
--- scripts/generate.py
+++ scripts/generate.py
@@ -7,7 +7,7 @@ src_path = f"{sys.path[0]}/../src/" # TODO: This probs won't work for everyone
 day = 1
 
 try:
-    # TODO: this will die on generating day 11. FIx me please
+    # TODO: this will die on generating day 11. Fix me please
     day = int(sorted(glob.glob(f"{src_path}day*"), reverse=True)[0][-1]) + 1
 
     if math.isnan(day):
@@ -21,10 +21,13 @@ day_path = os.path.join(src_path, day_name)
 os.mkdir(day_path)
 
 def create_class(name, item):
-    pass
+    f = open(f"{day_path}/{name}.py", "w")
+    f.write(f"class {name}():\n"
+            f"    def __init__(self):\n"
+            f"        pass")
 
 def create_function(name, item):
-    f = open(f"{day_path}{name}.py", "w")
+    f = open(f"{day_path}/{name}.py", "w")
     f.write(f"def {item['fn_name']}({item['fn_args']}) -> {item['return_type']}:\n"
             f"    pass")
     f.close()
@@ -34,9 +37,7 @@ for ds in CONFIG["dsa"]:
     if not item:
         raise Exception(f"algorithm {ds} could not be found")
 
-    print(item)
-    # TODO: we need to finish the config first before doing this
-#    if item["type"] == "class":
-#        create_class(ds, item)
-#    else:
-#        create_function(ds, item)
+    if item["type"] == "class":
+        create_class(ds, item)
+    else:
+        create_function(ds, item)
