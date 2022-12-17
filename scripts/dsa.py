c69b0ecc5e9c0a61355b989b4bfd3423cc4d9b54
theoboldalex <theoboldalex@gmail.com>
2022-12-17 21:31:22 +0000

    Adds DSAs for sorting algorithms

--- scripts/dsa.py
+++ scripts/dsa.py
@@ -1,29 +1,44 @@
 DSA = {
-    "ArrayList": {
-        "type": "class"
-    },
     "LinearSearchList": {
         "type": "fn",
         "fn_name": "linear_search",
-        "fn_args": "nums: list, target: int",
+        "fn_args": "haystack: list, needle: int",
         "return_type": "bool"
     },
     "BinarySearchList": {
         "type": "fn",
         "fn_name": "binary_search",
-        "fn_args": "nums: list, target: int",
+        "fn_args": "haystack: list, needle: int",
         "return_type": "bool"
     },
     "TwoCrystalBalls": {
         "type": "fn",
         "fn_name": "two_crystal_balls",
-        "fn_args": "nums: list",
+        "fn_args": "breaks: list",
         "return_type": "int"
     },
     "BubbleSort": {
         "type": "fn",
         "fn_name": "bubble_sort",
-        "fn_args": "nums: list",
-        "return_type": "list"
+        "fn_args": "arr: list",
+        "return_type": "None"
+    },
+    "InsertionSort": {
+        "type": "fn",
+        "fn_name": "insertion_sort",
+        "fn_args": "arr: list",
+        "return_type": "None"
+    },
+    "MergeSort": {
+        "type": "fn",
+        "fn_name": "merge_sort",
+        "fn_args": "arr: list",
+        "return_type": "None"
+    },
+    "QuickSort": {
+        "type": "fn",
+        "fn_name": "quick_sort",
+        "fn_args": "arr: list",
+        "return_type": "None"
     }
 }
